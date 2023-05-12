cfc7317f2280ac3a7db375169a047a3299fc4d5c
Charles Wolfe <cwolfe7@asu.edu>
2023-05-12 07:12:48 -0700

    sprites flicker when taking damage

--- Assets/scripts/Health.cs
+++ Assets/scripts/Health.cs
@@ -7,6 +7,7 @@ public class Health : MonoBehaviour
     [SerializeField] protected float hitPoints = 20f;
 
     protected Animator animator;
+    protected SpriteRenderer renderer;
  
 
     // Start is called before the first frame update
@@ -18,6 +19,7 @@ public class Health : MonoBehaviour
     protected virtual void Initialize()
     {
         animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     
@@ -33,6 +35,7 @@ public class Health : MonoBehaviour
         if (hitPoints <= 0) return;
 
         hitPoints -= damage;
+        PlayDamageEffect();
 
         if (hitPoints <= 0)
         {
@@ -43,5 +46,27 @@ public class Health : MonoBehaviour
     private void Die()
     {
         animator.SetTrigger("die");
+    }
+
+    private void PlayDamageEffect()
+    {
+        StartCoroutine(FlickerSprite(10));
+    }
+
+    // parameter flickerRate must be even number
+    IEnumerator FlickerSprite(int flickerRate)
+    {
+        Color c = renderer.material.color;
+
+        float alpha = 1f;
+
+        for (int i=0; i < flickerRate; i++)
+        {
+            alpha = (i % 2 == 0) ? 0.5f : 1f;
+            c.a = alpha;
+            renderer.material.color = c;
+            yield return new WaitForSeconds(1.0f / flickerRate);
+        }
+
     }
 }
