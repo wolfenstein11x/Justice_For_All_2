81d90eca1747ce6abaca175bce6241409a21d266
Charles Wolfe <cwolfe7@asu.edu>
2023-05-12 20:36:07 -0700

    added ammo count UI

--- Assets/scripts/Player/PlayerController.cs
+++ Assets/scripts/Player/PlayerController.cs
@@ -42,7 +42,7 @@ public class PlayerController : MonoBehaviour
         FlipSprite();
 
         // for debugging only
-        //meleeAttacker.InMeleeRange();
+        meleeAttacker.InMeleeRange();
 
         
         
