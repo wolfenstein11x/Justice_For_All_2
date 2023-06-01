632153b07542040d054b6f28dfa6b066d2a0b258
Charles Wolfe <cwolfe7@asu.edu>
2023-05-31 22:06:39 -0700

    started making lever

---
+++ Assets/prefabs/props/animatedProps/lever/Lever.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : ObjectHealth
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    protected override void Die()
+    {
+        if (animator.GetBool("switchedOn")) return;
+
+        //breakSound.Play();
+        animator.SetBool("switchedOn", true);
+    }
+}
