1c95e7e3293490430ed687c070f2eb1d50357da2
Charles Wolfe <cwolfe7@asu.edu>
2023-06-30 13:57:09 -0700

    started making level 28

--- Assets/prefabs/props/animatedProps/fireplace/Fireplace.cs
+++ Assets/prefabs/props/animatedProps/fireplace/Fireplace.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Fireplace : MonoBehaviour
 {
+    [SerializeField] bool startOn = false;
+
     Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        if (startOn)
+        {
+            animator.SetBool("fireStarted", true);
+        }
     }
 
     // Update is called once per frame
